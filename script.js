991a39c50baa9a3bcc2f96664a3e1ed36df1a92f
wrublik93 <wrublik.93@mail.ru>
2020-03-13 12:35:23 +0300

    fix: change function for navbar

--- script.js
+++ script.js
@@ -5,8 +5,10 @@ const PORTFOLIO_IMAGES_BORDER = document.querySelector('.portfolio-images');
 
 
 MENU.addEventListener('click', (event) => {
-    MENU.querySelectorAll('a').forEach(el => el.classList.remove('selected'));
-    event.target.classList.add('selected')
+    if(event.target.tagName == 'A') {
+        MENU.querySelectorAll('a').forEach(el => el.classList.remove('selected'));
+        event.target.classList.add('selected')
+    }
 });
 
 PORTFOLIO_TABS.addEventListener('click', (event) => {
@@ -14,17 +16,18 @@ PORTFOLIO_TABS.addEventListener('click', (event) => {
         PORTFOLIO_TABS.querySelectorAll('button').forEach (el => el.classList.remove('selected'));
         event.target.classList.add('selected');
         let copy = [];
-        PORTFOLIO_IMAGES_BORDER.querySelectorAll('img').forEach(el => el.classList.remove('img'));
+        PORTFOLIO_IMAGES_BORDER.querySelectorAll('img').forEach(el => el.classList.remove('portfolio-image-border'));
         PORTFOLIO_IMAGES.forEach(el => copy.push(el.src));
         copy.push(copy[0]);
         copy.splice(0,1);
         copy.forEach((el, index) => PORTFOLIO_IMAGES[index].src = el);
     }
-})
+});
 
 PORTFOLIO_IMAGES_BORDER.addEventListener('click', (event)=>{
-    if(event.target.classList[0] !== 'img' && event.target.tagName == 'IMG') {
-        PORTFOLIO_IMAGES_BORDER.querySelectorAll('img').forEach(el => el.classList.remove('img'));
-        event.target.classList.add('img')   
+    if(event.target.classList[0] !== 'portfolio-image-border' && event.target.tagName == 'IMG') {
+        PORTFOLIO_IMAGES_BORDER.querySelectorAll('img').forEach(el => el.classList.remove('portfolio-image-border'));
+        event.target.classList.add('portfolio-image-border')   
     }
-})
+});
+
