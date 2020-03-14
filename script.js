ef3bb1ea90b6cd7acc46bc2ba5c758dec41815c2
wrublik93 <wrublik.93@mail.ru>
2020-03-15 00:00:19 +0300

    feat: change active links when page scrolled

--- script.js
+++ script.js
@@ -1,50 +1,91 @@
+//----------------------------
 //constants
+//----------------------------
 const NAVBAR_LIST = document.querySelector('.navbar-list');
 const PORTFOLIO_TABS = document.querySelector('.filter');
 const PORTFOLIO_IMAGES = document.querySelectorAll('.portfolio-images img');
 const PORTFOLIO_IMAGES_BORDER = document.querySelector('.portfolio-images');
 const GET_QUOTES = document.querySelector('.quotes');
 const GET_QUOTE_FORM_SUBMIT = document.querySelector('.contact-form .contact-form-submit');
+
+//----------------------------
+//SECTIONS SITE
+//----------------------------
 const HEADER = document.querySelector('.header');
 const SLIDER = document.querySelector('.slider');
 const SERVICES = document.querySelector('.services');
 const PORTFOLIO = document.querySelector('.portfolio');
 const ABOUT_US = document.querySelector('.about-us');
 const CONTACT = document.querySelector('.quotes');
+const FOOTER = document.querySelector('.footer');
 
+//----------------------------
+//LINKS NAVBAR
+//----------------------------
+const HOME_LINK = document.querySelector('.navbar-list-item-home');
+const SERVICES_LINK = document.querySelector('.navbar-list-item-services');
+const PORTFOLIO_LINK = document.querySelector('.navbar-list-item-portfolio');
+const ABOUT_LINK = document.querySelector('.navbar-list-item-about');
+const CONTACT_LINK = document.querySelector('.navbar-list-item-contact');
+
+//----------------------------
+//DELETE HASH AFTER REFRESH PAGE
+//----------------------------
 window.onload = function() {
     window.location.hash = '';
 };
 
-//addEventListeners
-
+//----------------------------
 // 1) task with header and active links
+//----------------------------
 NAVBAR_LIST.addEventListener('click', (event) => {
     if(event.target.tagName == 'A') {
         NAVBAR_LIST.querySelectorAll('a').forEach(el => el.classList.remove('selected'));
         event.target.classList.add('selected');
         if(event.target.href.includes('services')) {
-            window.scroll(0, SERVICES.offsetTop - HEADER.offsetHeight);
+            window.scroll(0, SERVICES.offsetTop - HEADER.offsetHeight + 2);
         };
         if(event.target.href.includes('portfolio')) {
-            window.scroll(0, PORTFOLIO.offsetTop - HEADER.offsetHeight);
+            window.scroll(0, PORTFOLIO.offsetTop - HEADER.offsetHeight + 2);
         };
         if(event.target.href.includes('about')) {
-            window.scroll(0, ABOUT_US.offsetTop - HEADER.offsetHeight);
+            window.scroll(0, ABOUT_US.offsetTop - HEADER.offsetHeight + 2);
         };
         if(event.target.href.includes('contact')) {
-            window.scroll(0, CONTACT.offsetTop - HEADER.offsetHeight);
+            window.scroll(0, CONTACT.offsetTop - HEADER.offsetHeight + 2);
         };
     };
 });
 
-/* window.addEventListener('scroll', (event) => {
-    if(window.scrollY > SERVICES.offsetTop - HEADER.offsetHeight) {
+//----------------------------
+// 1.1) change active links when scroll page  
+//----------------------------
+window.addEventListener('scroll', () => {
+    if(window.scrollY < SERVICES.offsetTop - HEADER.offsetHeight) {
         NAVBAR_LIST.querySelectorAll('a').forEach(el => el.classList.remove('selected'));
-        SERVICES.classList.add('selected');
-        
-    }
-}) */
+        HOME_LINK.classList.add('selected')
+    };
+    
+    if(window.scrollY >= SERVICES.offsetTop - HEADER.offsetHeight && window.scrollY < PORTFOLIO.offsetTop - HEADER.offsetHeight) {
+        NAVBAR_LIST.querySelectorAll('a').forEach(el => el.classList.remove('selected'));
+        SERVICES_LINK.classList.add('selected')
+    };
+    
+    if(window.scrollY >= PORTFOLIO.offsetTop - HEADER.offsetHeight) {
+        NAVBAR_LIST.querySelectorAll('a').forEach(el => el.classList.remove('selected'));
+        PORTFOLIO_LINK.classList.add('selected')
+    };
+    
+    if(window.scrollY >= ABOUT_US.offsetTop - HEADER.offsetHeight) {
+        NAVBAR_LIST.querySelectorAll('a').forEach(el => el.classList.remove('selected'));
+        ABOUT_LINK.classList.add('selected')
+    };
+    
+    if(window.scrollY >= CONTACT.offsetTop - HEADER.offsetHeight) {
+        NAVBAR_LIST.querySelectorAll('a').forEach(el => el.classList.remove('selected'));
+        CONTACT_LINK.classList.add('selected')
+    };
+})
 
 
 
