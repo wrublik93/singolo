93dc8e0c4dd4dac371faf701f07e8819db3d4504
wrublik93 <wrublik.93@mail.ru>
2020-03-14 20:50:05 +0300

    fix: fix overlay fixed header navigation

--- script.js
+++ script.js
@@ -1,15 +1,57 @@
-const MENU = document.querySelector('.navbar-list');
+//constants
+const NAVBAR_LIST = document.querySelector('.navbar-list');
 const PORTFOLIO_TABS = document.querySelector('.filter');
 const PORTFOLIO_IMAGES = document.querySelectorAll('.portfolio-images img');
 const PORTFOLIO_IMAGES_BORDER = document.querySelector('.portfolio-images');
+const GET_QUOTES = document.querySelector('.quotes');
+const GET_QUOTE_FORM_SUBMIT = document.querySelector('.contact-form .contact-form-submit');
+const HEADER = document.querySelector('.header');
+const SLIDER = document.querySelector('.slider');
+const SERVICES = document.querySelector('.services');
+const PORTFOLIO = document.querySelector('.portfolio');
+const ABOUT_US = document.querySelector('.about-us');
+const CONTACT = document.querySelector('.quotes');
 
+window.onload = function() {
+    window.location.hash = '';
+};
 
-MENU.addEventListener('click', (event) => {
+//addEventListeners
+
+// 1) task with header and active links
+NAVBAR_LIST.addEventListener('click', (event) => {
     if(event.target.tagName == 'A') {
-        MENU.querySelectorAll('a').forEach(el => el.classList.remove('selected'));
-        event.target.classList.add('selected')
-    }
+        NAVBAR_LIST.querySelectorAll('a').forEach(el => el.classList.remove('selected'));
+        event.target.classList.add('selected');
+        if(event.target.href.includes('services')) {
+            window.scroll(0, SERVICES.offsetTop - HEADER.offsetHeight);
+        };
+        if(event.target.href.includes('portfolio')) {
+            window.scroll(0, PORTFOLIO.offsetTop - HEADER.offsetHeight);
+        };
+        if(event.target.href.includes('about')) {
+            window.scroll(0, ABOUT_US.offsetTop - HEADER.offsetHeight);
+        };
+        if(event.target.href.includes('contact')) {
+            window.scroll(0, CONTACT.offsetTop - HEADER.offsetHeight);
+        };
+    };
 });
+
+/* window.addEventListener('scroll', (event) => {
+    if(window.scrollY > SERVICES.offsetTop - HEADER.offsetHeight) {
+        NAVBAR_LIST.querySelectorAll('a').forEach(el => el.classList.remove('selected'));
+        SERVICES.classList.add('selected');
+        
+    }
+}) */
+
+
+
+
+
+
+
 
 PORTFOLIO_TABS.addEventListener('click', (event) => {
     if(event.target.classList[1] !== 'selected' && event.target.tagName == 'BUTTON') {
@@ -31,3 +73,22 @@ PORTFOLIO_IMAGES_BORDER.addEventListener('click', (event)=>{
     }
 });
 
+GET_QUOTE_FORM_SUBMIT.addEventListener('click', (event) => {
+    /* event.preventDefault(); */
+    document.querySelector('.block-message').classList.remove('block-message-hidden')
+    //let contactFormName = document.querySelector('.contact-form .contact-form-name').value;
+    //let contactFormEmail = document.querySelector('.contact-form .contact-form-email').value;
+    let contactFormSubject = document.querySelector('.contact-form .contact-form-subject').value;
+    let contactFormDescription = document.querySelector('.contact-form .contact-form-description').value;
+    //console.log(contactFormName, contactFormEmail, contactFormSubject, contactFormDescription);
+    if(contactFormSubject !== '') {
+        document.querySelector('.message .block-message-subject').innerText = 'Тема: ' + contactFormSubject;
+    } else {
+        document.querySelector('.message .block-message-subject').innerText = 'Без темы'
+    };
+    if(contactFormDescription !== '') {
+        document.querySelector('.message .block-message-description').innerText = 'Описание: ' + contactFormDescription;
+    } else {
+        document.querySelector('.message .block-message-description').innerText = 'Без описания'
+    }
+});
